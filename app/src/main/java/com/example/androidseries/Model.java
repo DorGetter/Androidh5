a7c33b36304ed9dddf12f47db8619a2a01479899
eldar <eldartakach@gmail.com>
2020-12-11 13:31:02 +0200

    Bug fix

--- app/src/main/java/com/example/androidseries/Model.java
+++ app/src/main/java/com/example/androidseries/Model.java
@@ -1,11 +1,15 @@
 package com.example.androidseries;
-
+import org.deeplearning4j.nn.modelimport.keras.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.modelimport.keras.UnsupportedKerasConfigurationException;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.nd4j.linalg.io.ClassPathResource;
-
 import java.io.IOException;
 
 public class Model {
-    public Model() throws IOException {
+
+
+    public Model() throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
         String simpleMlp = new ClassPathResource("model.h5").getFile().getPath();
         MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights(simpleMlp);
     }
